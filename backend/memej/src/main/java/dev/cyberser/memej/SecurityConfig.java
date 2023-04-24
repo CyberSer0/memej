5933fd859883fc7588588b13b5f2581c5f93977c
CyberCheese <arturszalyga@wp.pl>
2023-04-24 02:03:24 +0200

    modified database fields

--- backend/memej/src/main/java/dev/cyberser/memej/SecurityConfig.java
+++ backend/memej/src/main/java/dev/cyberser/memej/SecurityConfig.java
@@ -4,8 +4,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+// @EnableWebSecurity
+// @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
     
 }
