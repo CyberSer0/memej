ae80d54d56773e28b8bd5c9c63bbd9fe7edc41c0
CyberSer <arturgames14@gmail.com>
2023-05-08 01:39:37 +0200

    [Backend]: Added working security prototype

--- backend/memej/src/main/java/dev/cyberser/memej/SecurityConfig.java
+++ backend/memej/src/main/java/dev/cyberser/memej/SecurityConfig.java
@@ -1,11 +1,10 @@
 package dev.cyberser.memej;
 
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-// @EnableWebSecurity
-// @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
     
 }
